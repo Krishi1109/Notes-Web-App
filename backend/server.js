2eec44edf83b24d6a88991dbf9e0688998945603
Krishi Tanna <68857159+Krishi1109@users.noreply.github.com>
2023-07-06 12:26:53 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -28,17 +28,17 @@ app.use("/api/users", userRoutes);
 app.use("/api/notes", noteRoutes);
 
 // ---------------- Deployment --------------------------
-// __dirname = path.resolve();
-// if (process.env.NODE_ENV == "production") {
-//   app.use(express.static(path.join(__dirname, "/frontend/build")));
-//   app.get('*', (req,res) => {
-//     res.sendFile(path.resolve(__dirname, "frontend", "build", 'index.html'))
-//   })
-// } else {
-//   app.get("/", (req, res) => {
-//     res.send("Done for the day");
-//   });
-// }
+__dirname = path.resolve();
+if (process.env.NODE_ENV == "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+  app.get('*', (req,res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", 'index.html'))
+  })
+} else {
+  app.get("/", (req, res) => {
+    res.send("Done for the day");
+  });
+}
 // ---------------- Deployment --------------------------
 
 app.use(notFound);
